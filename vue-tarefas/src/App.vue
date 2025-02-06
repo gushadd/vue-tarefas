1b0dd7a9630dc006bf797e79a3808b064924502c
Gustavo <gustavoeliashaddad@gmail.com>
2025-02-06 15:04:49 -0300

    feat: arquivos e configurações iniciais

---
+++ vue-tarefas/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+//import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  
+    <!-- <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav> -->
+
+</template>
+
+<style scoped>
+
+</style>
