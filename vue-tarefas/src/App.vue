33b9ae30d67a839977d37b5d70e937ef5266c2d8
Gustavo <gustavoeliashaddad@gmail.com>
2025-03-25 14:53:07 -0300

    feat: arquivos iniciais

---
+++ vue-tarefas/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import SideMenu from '../src/components/SideMenu.vue'
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+
+const changeTheme = () => {
+  document.documentElement.classList.toggle('app-dark');
+};
+
+</script>
+
+<template>
+  <SideMenu/>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
